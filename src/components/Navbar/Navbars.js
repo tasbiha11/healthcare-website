86fd76abc9c32f9a5784e5c7ae3c5faaad7b4439
tasbiha11 <tasbihahussain@yahoo.com>
2023-01-27 12:47:29 +0600

    navCSS

--- src/components/Navbar/Navbars.js
+++ src/components/Navbar/Navbars.js
@@ -3,16 +3,20 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown'
+import './Nav.css';
+import logo from '../../Assets/logo.png'
 
 const Navbars = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar expand="lg">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand>
+                    <img src={logo} title="logo" />
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link className="active">Home</Nav.Link>
                         <NavDropdown title="Pages" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">About Us</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.1">Our Team</NavDropdown.Item>
@@ -30,6 +34,9 @@ const Navbars = () => {
                             <NavDropdown.Item href="#action/3.1">Blog Details</NavDropdown.Item>
                         </NavDropdown>
                         <Nav.Link href="#home">Contact Us</Nav.Link>
+                        <Nav.Link href="#home">search</Nav.Link>
+                        <Nav.Link href="#home">icon + 01780311229</Nav.Link>
+                        <Nav.Link href="#home"><button>Contact us <span>&gt;</span> </button></Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
